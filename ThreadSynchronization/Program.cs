7a1473305d869044ea064077e9b9695c2fabe75a
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-05-13 16:35:25 +0800

    AutoResetEvent/ManualResetEvent

--- ThreadSynchronization/Program.cs
+++ ThreadSynchronization/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading;  
 
 namespace ThreadSynchronization
 {
     class Program
     {
+        static AutoResetEvent autoEvent;  
+
+        static void DoWork()  
+        {  
+            Console.WriteLine("   worker thread started, now waiting on event...");  
+            autoEvent.WaitOne();  
+            Console.WriteLine("   worker thread reactivated, now exiting...");  
+        }  
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            autoEvent = new AutoResetEvent(false);  
+
+            Console.WriteLine("main thread starting worker thread...");  
+            Thread t = new Thread(DoWork);  
+            t.Start();  
+
+            Console.WriteLine("main thread sleeping for 1 second...");  
+            Thread.Sleep(1000);  
+
+            Console.WriteLine("main thread signaling worker thread...");  
+            autoEvent.Set();  
+
         }
     }
 }
