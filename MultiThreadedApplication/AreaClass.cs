5952b008b9baf873bd830a94ddff180babb4746f
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-05-13 15:13:23 +0800

    Component Model

---
+++ MultiThreadedApplication/AreaClass.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MultiThreadedApplication
+{
+    public class AreaClass
+    {  
+        public double Base;  
+        public double Height;  
+        public double CalcArea()  
+        {  
+            // Calculate the area of a triangle.  
+            return 0.5 * Base * Height;  
+        }  
+    }
+}
---
+++ MultiThreadedApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MultiThreadedApplication
+{
+    class Program
+    {
+            private static System.ComponentModel.BackgroundWorker BackgroundWorker1  
+    = new System.ComponentModel.BackgroundWorker();  
+
+        static void Main(string[] args)
+        {
+            InitializeBackgroundWorker();  
+
+            AreaClass AreaObject = new AreaClass();  
+            AreaObject.Base = 30;  
+            AreaObject.Height = 40;  
+
+            // Start the asynchronous operation.  
+            BackgroundWorker1.RunWorkerAsync(AreaObject); 
+
+        }
+
+        private static void InitializeBackgroundWorker()  
+        {  
+            // Attach event handlers to the BackgroundWorker object.  
+            BackgroundWorker1.DoWork +=  
+                new System.ComponentModel.DoWorkEventHandler(BackgroundWorker1_DoWork);  
+            BackgroundWorker1.RunWorkerCompleted +=  
+                new System.ComponentModel.RunWorkerCompletedEventHandler(BackgroundWorker1_RunWorkerCompleted);  
+        }  
+
+        private static void BackgroundWorker1_DoWork(  
+            object sender,  
+            System.ComponentModel.DoWorkEventArgs e)  
+        {  
+            AreaClass AreaObject2 = (AreaClass)e.Argument;  
+            // Return the value through the Result property.  
+            e.Result = AreaObject2.CalcArea();  
+        }  
+
+        private static void BackgroundWorker1_RunWorkerCompleted(  
+            object sender,  
+            System.ComponentModel.RunWorkerCompletedEventArgs e)  
+        {  
+            // Access the result through the Result property.  
+            double Area = (double)e.Result;  
+            Console.WriteLine("The area is: " + Area.ToString());  
+        }  
+    }
+}
